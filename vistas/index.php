8aced9e8a2a87a9a236e8c4b9686d29811f24939
Juan Carlos <jlujanog@toluca.tecnm.mx>
2020-06-08 18:54:40 -0500

    index.php

---
+++ vistas/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Imagen</title>
+    </head>
+    
+    <body>
+        <h1>titulo nivel 1</h1>
+        <p>parrafo de texto. <strong>negrita</strong> y <em>cursiva</em></p>
+
+        <img src="resource/img.jpg" width="400" height="492" alt="shazam">
+    </body>
+
+</html>
