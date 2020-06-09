4647c49cf88a5d1174db2a28d683cbcc973e2647
Juan Carlos <jlujanog@toluca.tecnm.mx>
2020-06-08 20:07:27 -0500

    otro

--- vistas/index.php
+++ vistas/index.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <title>Imagen</title>
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
 
     </head>
     
@@ -14,24 +14,12 @@
                 <a href="index.php" class="navbar-brand">Index.com</a>
    
               <div class="dropdown">
-                    <a class="btn btn-secondary dropdown-toggle" href="#" id="dropdown01" data-toggle="dropdown" >Crud</a>
+                    <a class="btn btn-secondary dropdown-toggle" href="#" id="dropdown01" data-toggle="dropdown" >Paginas</a>
                     <div class="dropdown-menu" aria-labelledby="dropdown01">
                         <a class="dropdown-item" href="operadores.html">Operadores</a>
-                        <a class="dropdown-item disabled" href="#" tabindex="-1" aria-disabled="true">EDITAR</a>
-                        <a class="dropdown-item disabled" href="#" tabindex="-1" aria-disabled="true">ELIMINAR</a>
                         <a class="dropdown-item" href="primerJS.html">PrimerJS.html</a>
                         <a class="dropdown-item" href="index.php">INDEX</a>
-                     </div>
-                 </div>
-
-
-                <div class="nav-item dropdown">
-                     <a class="nav-link dropdown-toggle" href="#" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Menú</a>
-                    <div class="dropdown-menu" aria-labelledby="dropdown01">
-                        <a class="dropdown-item" href="login.php">Cerrar Secion</a>
-                        <a class="dropdown-item" href="#">Acerca de</a>
-                        <a class="dropdown-item" href="#">Autor</a>
-                    </div>
+                     </div>    
                 </div>
             </div>
         </nav>
@@ -45,9 +33,9 @@
 
     </body>
     <script src="https://code.jquery.com/jquery-1.11.1.min.js"></script>
-  <script src="https://kit.fontawesome.com/ca583de3be.js" crossorigin="anonymous"></script>
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/ca583de3be.js" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
 
 </html>
