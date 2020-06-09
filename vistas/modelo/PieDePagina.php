b67bd9c6e24d187aadc90cdffc7f3928495620a3
LeonardoA <larroyos1@toluca.tecnm.mx>
2020-06-09 14:16:35 -0500

    Se sube los tamplates del ejemplo
    añadi los templates de la aplicacion ninja pizza

---
+++ vistas/modelo/PieDePagina.php
@@ -0,0 +1,17 @@
+
+    </div>
+    <!-- /.container -->
+
+    <!-- Footer -->
+    <footer class="py-5">
+        
+        <div class="container">
+        
+            <p class="m-0 text-center text-secondary">Copyright &copy; Ninja Pizza 2019</p>
+        
+        </div>
+        <!-- /.container -->
+    </footer>
+
+</body>
+
